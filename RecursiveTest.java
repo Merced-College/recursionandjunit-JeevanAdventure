e49dfaa2786c952b72b866dbbd0a1c7edbe708ad
JeevanAdventure <86693429+JeevanAdventure@users.noreply.github.com>
2024-11-08 21:30:41 +0000

    Added Demo code to classes

---
+++ RecursiveTest.java
@@ -0,0 +1,9 @@
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class RecursiveTest {
+    @Test
+    public void test1() {
+        assertEquals(main.count8(88188),5);
+    }
+}
